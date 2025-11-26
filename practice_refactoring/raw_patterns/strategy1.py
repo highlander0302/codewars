6046d0704245aceca100fbf6b36d3cefcdaaf5a0
TheApp Team <noreply@example.com>
2025-11-26 17:47:00 +0100

    refactor(raw_patterns/strategy) refactored the problem strategy1 to practice the pattern

--- practice_refactoring/raw_patterns/strategy1.py
+++ practice_refactoring/raw_patterns/strategy1.py
@@ -1,26 +1,53 @@
 """This code wouldbenefit from strategy pattern implementation."""
 
+from abc import ABC, abstractmethod
+
+
+class PaymentType(ABC):
+    """Interface for payments."""
+
+    @abstractmethod
+    def checkout(self, amount: float) -> str: ...
+
+
+class CreditCard(PaymentType):
+    """Concrete implementation for credit card payments"""
+
+    def checkout(self, amount: float):
+        return f"Paid {amount} by Credit Card"
+
+
+class PayPal(PaymentType):
+    """Concrete implementation for Pay Pal payments"""
+
+    def checkout(self, amount: float):
+        return f"Paid {amount} by Pay Pal"
+
+
+class Bitcoin(PaymentType):
+    """Concrete implementation for Bitcoin payments"""
+
+    def checkout(self, amount: float):
+        return f"Paid {amount} by Bitcoin"
+
 
 class ShoppingCart:
-    def __init__(self, payment_type):
+    """Client for shopping chart actions"""
+
+    def __init__(self, payment_type: PaymentType):
         self.payment_type = payment_type
 
-    def checkout(self, amount):
-        if self.payment_type == "credit_card":
-            print(f"Paying {amount} using Credit Card")
-        elif self.payment_type == "paypal":
-            print(f"Paying {amount} using PayPal")
-        elif self.payment_type == "bitcoin":
-            print(f"Paying {amount} using Bitcoin")
-        else:
-            print(f"Payment type {self.payment_type} not supported")
+    def checkout(self, amount: float) -> None:
+        print(self.payment_type.checkout(amount))
 
 
-cart1 = ShoppingCart("credit_card")
-cart1.checkout(100)
+if __name__ == "__main__":
+    base_payment_type = CreditCard()
+    cart = ShoppingCart(base_payment_type)
+    cart.checkout(100)
 
-cart2 = ShoppingCart("paypal")
-cart2.checkout(200)
+    cart.payment_type = PayPal()
+    cart.checkout(200)
 
-cart3 = ShoppingCart("bitcoin")
-cart3.checkout(300)
+    cart.payment_type = Bitcoin()
+    cart.checkout(300)
