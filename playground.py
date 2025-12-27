1a68baefe446dfef009fadda218c4fc9de4c3fe6
highlander <highlander@gmail.com>
2025-12-27 16:04:57 +0100

    practice(classes) practice classes creation inside playground.py

--- playground.py
+++ playground.py
@@ -1,17 +1,45 @@
-from functools import wraps
-from typing import Any, Callable
+"""Now I want to create a class"""
+
+from abc import ABC, abstractmethod
 
 
-def before_after_decorator(func: Callable[[str], None]) -> Callable[[str], None]:
-    @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any):
-        print("before hello")
-        func(*args, **kwargs)
-        print("after hello")
+class Engine(ABC):
+    """Interface for general Engine."""
 
-    return wrapper
+    def __init__(self, power: int, weight: int) -> None:
+        self.power = power
+        self.weight = weight
+
+    @abstractmethod
+    def start(self) -> None: ...
+
+    @abstractmethod
+    def stop(self) -> None: ...
 
 
-@before_after_decorator
-def say_hallo(name: str) -> None:
-    print(f"hey to {name}!")
+class DiselEngine(Engine):
+    """Concrete class that describes disel engine."""
+
+    def __init__(self, power: int, weight: int, disel_consumption: int) -> None:
+        super().__init__(power, weight)
+        self.disel_consumption = disel_consumption
+
+    def start(self) -> None:
+        print("Engine started.")
+
+    def stop(self) -> None:
+        print("Engine stopped.")
+
+
+class BioDiselEngine(Engine):
+    """Concrete class that describes engine that workse on biodisel."""
+
+    def __init__(self, power: int, weight: int, biodisel_consumption: int) -> None:
+        super().__init__(power, weight)
+        self.biodisel_consumption = biodisel_consumption
+
+    def start(self) -> None:
+        print("Engine started.")
+
+    def stop(self) -> None:
+        print("Engine stopped.")
